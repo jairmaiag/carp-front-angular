da066cf2831cb32b10eb1375e3ff4933002bbb7c
jairmaiag <jairmaiag@gmail.com>
2020-06-11 13:08:22 -0300

    Adicionando rota de login e de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
 import { AcessoComponent } from "./application/acesso/acesso.component";
 
-const routes: Routes = [{ path: "login", component: AcessoComponent }];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: AcessoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
