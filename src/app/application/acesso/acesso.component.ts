709bd68d6682c26a5b25cd3e2069a7a92200f2f6
jairmaiag <jairmaiag@gmail.com>
2020-06-12 10:15:09 -0300

    Criação do componente de Pessoa

--- src/app/application/acesso/acesso.component.ts
+++ src/app/application/acesso/acesso.component.ts
@@ -1,5 +1,8 @@
-import { Usuario } from "./../../model/usuario";
 import { Component, OnInit } from "@angular/core";
+
+import * as $ from "jquery";
+
+import { Usuario } from "./../../model/usuario";
 
 @Component({
   selector: "acesso",
@@ -8,7 +11,10 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AcessoComponent implements OnInit {
   usuario: Usuario;
-  constructor() {}
+  constructor() {
+    $("body").addClass("bodyAcesso");
+    $("#barraMenu").hide();
+  }
 
   ngOnInit(): void {}
   login(): void {}
--- src/app/application/pessoa/pessoa.component.ts
+++ src/app/application/pessoa/pessoa.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Pessoa } from "./../../model/pessoa";
+import { PessoaService } from "./../../service/pessoa.service";
 
 @Component({
-  selector: 'app-pessoa',
-  templateUrl: './pessoa.component.html',
-  styleUrls: ['./pessoa.component.css']
+  selector: "app-pessoa",
+  templateUrl: "./pessoa.component.html",
+  styleUrls: ["./pessoa.component.css"],
 })
 export class PessoaComponent implements OnInit {
+  pessoas: Pessoa[];
 
-  constructor() { }
+  constructor(private serivco: PessoaService) {}
 
   ngOnInit(): void {
+    this.serivco.listar().subscribe(dados => this.pessoas = dados);
   }
-
 }
