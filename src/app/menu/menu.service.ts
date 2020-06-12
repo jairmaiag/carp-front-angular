544142cdeaa02d64006bc5a5b1458b2f8e5d845b
jairmaiag <jairmaiag@gmail.com>
2020-06-12 16:39:33 -0300

    Ajuste do link para Ordem de serviço

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -25,7 +25,12 @@ export class MenuService {
     let itensMenu: Array<Item> = [
       { id: 1, texto: "Home", link: "/", subIntens: null },
       { id: 2, texto: "Orçamento", link: "/orcamento", subIntens: null },
-      { id: 3, texto: "Ordem de Serviço", link: "/os", subIntens: null },
+      {
+        id: 3,
+        texto: "Ordem de Serviço",
+        link: "/ordemservico",
+        subIntens: null,
+      },
       itemTabela,
       { id: 5, texto: "Login", link: "/login", subIntens: null },
     ];
