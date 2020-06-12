0747601c183c56042160c8c330eafa79abcd8098
jairmaiag <jairmaiag@gmail.com>
2020-06-12 10:15:23 -0300

    Criação do serviço de Pessoa

--- src/app/service/pessoa.service.ts
+++ src/app/service/pessoa.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pessoa } from '../model/pessoa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
+  private readonly API = 'http://localhost/pessoa';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  listar(){
+     return this.http.get<Pessoa[]>(this.API);
+  }
 }
