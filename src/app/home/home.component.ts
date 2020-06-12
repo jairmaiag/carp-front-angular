ae2c60449af08ac272461bcda6d2fc869b63fa45
jairmaiag <jairmaiag@gmail.com>
2020-06-12 17:19:24 -0300

    Injeção do Serviço de acesso no componente Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+import { AcessoService } from "./../service/acesso.service";
+
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private acesso: AcessoService) {}
 
   ngOnInit(): void {}
 }
