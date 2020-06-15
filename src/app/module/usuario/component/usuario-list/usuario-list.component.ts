9b6b4d2da9bd48a39e05a730b920ac01621d7f42
jairmaiag <jairmaiag@gmail.com>
2020-06-15 16:39:58 -0300

    Criação do componente de listagem de usuário dentro do módulo de usuário.

---
+++ src/app/module/usuario/component/usuario-list/usuario-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Observable } from "rxjs";
+
+import { Usuario } from "./../../model/usuario";
+import { UsuarioService } from "src/app/service/usuario.service";
+
+@Component({
+  selector: "app-usuario-list",
+  templateUrl: "./usuario-list.component.html",
+  styleUrls: ["./usuario-list.component.css"],
+})
+export class UsuarioListComponent implements OnInit {
+  lista$: Observable<Usuario[]>;
+  totalRegistro: number;
+
+  constructor(private servico: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.lista$ = this.servico.getLista();
+  }
+}
