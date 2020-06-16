8443d10460abd7bbd3741bd03ee222754da0a392
jairmaiag <jairmaiag@gmail.com>
2020-06-16 13:59:47 -0300

    Utilização do AcessoGuar para não permitir acesso aos links sem estarem logados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,15 @@ import { FuncionarioComponent } from "./application/funcionario/funcionario.comp
 import { PessoaComponent } from "./application/pessoa/pessoa.component";
 import { OrcamentoComponent } from "./application/orcamento/orcamento.component";
 import { OrdemservicoComponent } from "./application/ordemservico/ordemservico.component";
+import { AcessoGuard } from "./guards/acesso.guard";
 
 const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: HomeComponent,
+    canActivate: [AcessoGuard],
+  },
   {
     path: "login",
     loadChildren: () =>
@@ -22,19 +29,31 @@ const routes: Routes = [
       import("./module/usuario/usuario.module").then(
         (mod) => mod.UsuarioModule
       ),
+    canActivate: [AcessoGuard],
   },
-  { path: "", pathMatch: "full", component: HomeComponent },
-  { path: "funcionario", component: FuncionarioComponent },
-  { path: "cliente", component: ClienteComponent },
+  {
+    path: "funcionario",
+    component: FuncionarioComponent,
+    canActivate: [AcessoGuard],
+  },
+  { path: "cliente", component: ClienteComponent, canActivate: [AcessoGuard] },
   { path: "fornecedor", component: FornecedorComponent },
-  { path: "produto", component: ProdutoComponent },
-  { path: "pessoa", component: PessoaComponent },
-  { path: "orcamento", component: OrcamentoComponent },
-  { path: "ordemservico", component: OrdemservicoComponent },
+  { path: "produto", component: ProdutoComponent, canActivate: [AcessoGuard] },
+  { path: "pessoa", component: PessoaComponent, canActivate: [AcessoGuard] },
+  {
+    path: "orcamento",
+    component: OrcamentoComponent,
+    canActivate: [AcessoGuard],
+  },
+  {
+    path: "ordemservico",
+    component: OrdemservicoComponent,
+    canActivate: [AcessoGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
