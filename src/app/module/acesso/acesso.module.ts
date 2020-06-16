c966b933b3cc2ea92893e3e30283e4ada2a3cf26
jairmaiag <jairmaiag@gmail.com>
2020-06-16 14:04:30 -0300

    Criação das rotas de acesso e a inclusão do FormsModule

--- src/app/module/acesso/acesso.module.ts
+++ src/app/module/acesso/acesso.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { UsuarioModule } from "../usuario/usuario.module";
 
+import { AcessoRoutingModule } from "./acesso-routing.module";
 import { AcessoService } from "./serivce/acesso.service";
 import { AcessoComponent } from "./component/acesso/acesso.component";
 
 @NgModule({
   declarations: [AcessoComponent],
-  imports: [CommonModule, UsuarioModule],
+  imports: [CommonModule, FormsModule, AcessoRoutingModule, UsuarioModule],
   providers: [AcessoService],
-  exports: [AcessoService, AcessoComponent],
+  bootstrap: [AcessoComponent],
 })
-export class AcessoModule {}
+export class AcessoModule { }
