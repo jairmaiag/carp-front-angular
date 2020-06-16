5bc24324a1251c7a7129e8f52c5eea58b5fdd51e
jairmaiag <jairmaiag@gmail.com>
2020-06-16 14:00:10 -0300

    Limpeza de código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AcessoService } from "./module/acesso/serivce/acesso.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'carp-front';
+export class AppComponent implements OnInit {
+  title = "carp-front";
+  usuarioLogado: boolean = false;
+
+  constructor(private acessoSevice: AcessoService) {}
+  ngOnInit() {
+    AcessoService.logado.subscribe(
+      (logado) => (this.usuarioLogado = logado)
+    );
+  }
 }
