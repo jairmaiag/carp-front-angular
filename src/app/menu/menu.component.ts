3728f7dcb941d2b6e3544130de1920ae1ca0c708
jairmaiag <jairmaiag@gmail.com>
2020-06-16 14:02:58 -0300

    Utilização od servido de AcessoService para exibição ou não da barra de menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,30 +1,43 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 import * as $ from "jquery";
 
 import { MenuService } from "./menu.service";
 import { Item } from "./item";
+import { AcessoService } from '../module/acesso/serivce/acesso.service';
 
 @Component({
   selector: "menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.css"],
-  providers: [MenuService],
+  providers: [MenuService, AcessoService],
 })
 export class MenuComponent implements OnInit {
-  servico: MenuService;
-  itensMenu: Array<Item>;
+  private rotador: Router;
+  private itensMenu: Array<Item>;
+  private logado: boolean = false;
 
-  constructor(servico: MenuService) {
+  constructor(private servico: MenuService) {
     this.servico = servico;
+  }
+
+  ngOnInit(): void {
+    AcessoService.logado.subscribe(
+      logado => this.logado = logado
+    );
     this.itensMenu = this.servico.getMenu();
     $(".nav-item").each(function (posicao, item) {
       item.onclick = function () {
-        var $listaMenu = $("#intensBarraMenu");
+        const $listaMenu = $("#intensBarraMenu");
         $listaMenu.removeClass("show");
       };
     });
   }
-
-  ngOnInit(): void {}
+  getItensMenu(): Array<Item> {
+    return this.itensMenu;
+  }
+  getLogado(): boolean {
+    return this.logado;
+  }
 }
